35abaaa2a324dc7443a362c418cd66e3a6ffd200
boraozer <nagayapim@gmail.com>
2020-08-21 22:46:22 +0300

    new database enums add

--- src/enums/database-enum.js
+++ src/enums/database-enum.js
@@ -1,3 +1,5 @@
 module.exports = {
-    PROCESS_COMPLATED : 'Document successfull inserted.'
+    PROCESS_COMPLATED : 'Process successfull.',
+    NOT_FOUND_DOCUMENT_BY_REF : 'Document not found in local database.',
+    PROCESS_NOT_COMPLATED : 'Process not success.'
 }
