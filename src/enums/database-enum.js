ebef5136514a2e1baad3d2fc6f6518af19fb9c2a
boraozer <nagayapim@gmail.com>
2020-12-23 00:59:34 +0300

    deleted all file ready a new version!

--- src/enums/database-enum.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    PROCESS_COMPLATED : 'Process successfull.',
-    NOT_FOUND_DOCUMENT_BY_REF : 'Document not found in local database.',
-    PROCESS_NOT_COMPLATED : 'Process not success.'
-}
--- src/enums/network-enum.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    P2P_CONNECTED : "P2P Connection ready!",
-    QUERY_SIGNAL : 'query-signal',
-    SOCKET_EVENT_TYPE_MISSING : 'Socket message missing eventType'
-};
