6c399dbb7c2128a23f501a40f0c8f6c2ef970cb8
boraozer <nagayapim@gmail.com>
2020-12-27 22:19:40 +0300

    update package version & integration schema

--- collections.js
+++ collections.js
@@ -5,16 +5,62 @@ export default [
             'title',
             'content'
         ],
-        ref : 'documentId'
+        ref : 'documentId',
+        schema : {
+            title : {
+                type : 'string',
+                min : 3,
+                max : 80,
+                required : true
+            },
+            content : {
+                type : 'string',
+                min : 7,
+                max : 500,
+                required : true
+            },
+            documentId : {
+                createField : ['title', 'content']
+            }   
+        }
     },
     {
         collectionName : 'webPage',
         fields : [
             'title',
             'description',
-            'keywords',
-            'domain'
+            'domain',
+            'url'
         ],
-        ref : 'documentId'
+        ref : 'documentId',
+        schema : {
+            title : {
+                type : 'string',
+                min : 4,
+                max : 70,
+                required : true
+            },
+            description : {
+                type : 'string',
+                min : 7,
+                max : 255,
+                required : true
+            },
+            url : {
+                type : 'string',
+                min : 6,
+                max : 175,
+                required : true
+            },
+            domain : {
+                type : 'string',
+                min : 6,
+                max : 35,
+                required : true
+            },
+            documentId : {
+                createField : ['url']
+            }   
+        }
     }
 ]
