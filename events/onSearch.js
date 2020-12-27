50e432694503d5ef0dca3a33b462d78356b62f8c
boraozer <nagayapim@gmail.com>
2020-12-27 18:26:40 +0300

    finished native events middleware

--- events/onSearch.js
+++ events/onSearch.js
@@ -1,8 +1,27 @@
+import middleware from '../core/middleware.js';
+
+const eventMiddleware = new middleware({
+    timeout : 1000, // miliseconds
+    warningLimit : 5
+});
+
+
 const name = 'onSearch';
 
 
 async function listener(data)
 { 
+    const by = data._by;
+    eventMiddleware.up(by);
+    const status = eventMiddleware.status(by);
+
+    if(status.warning){
+        if(this.peer.connections[by] !== undefined) {
+            this.dropPeer(by);
+        }
+        return;
+    }
+
     const targetCollections = data.collections;
 
     let resultMap = {};
@@ -12,9 +31,7 @@ async function listener(data)
         resultMap[collection] = findCollection.search(data.query)
     });
 
-    const to = data._by;
-
-    this.peer.send(to, {
+    this.peer.send(by, {
         listener : data.listener,
         data :{
             results : resultMap
