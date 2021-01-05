8a811730dfda759a0b2b58441708904d877109b3
boraozer <nagayapim@gmail.com>
2021-01-06 00:52:10 +0300

    tokenization method changed

--- index.js
+++ index.js
@@ -43,7 +43,16 @@ class foxql {
             this.currentCollections.push(collectionName);
 
             this.database.useCollection(collectionName).registerAnalyzer('tokenizer', (string)=>{
-                return string.toLowerCase().replace(/[^\w\s]/gi, ' ').replace(/  +/g, ' ').trim();
+                return string.toLowerCase().
+                replace(/ö/gi, 'o').
+                replace(/ç/gi, 'c').
+                replace(/ş/gi, 's').
+                replace(/ğ/gi, 'g').
+                replace(/ü/gi, 'u').
+                replace(/ı/gi, 'i').
+                replace(/[^\w\s]/gi, ' ').
+                replace(/  +/g, ' ').
+                trim();
             });
         })
     }
