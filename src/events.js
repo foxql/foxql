8121cb61803de6fa2f9e3180917203b97ae4ee6d
boraozer <nagayapim@gmail.com>
2021-05-16 05:22:12 +0300

    onSync event listener name changed to onOfferedDocuments

--- src/events.js
+++ src/events.js
@@ -2,7 +2,7 @@ import onDocument from './events/onDocument.js';
 import onSearch from './events/onSearch.js';
 import onRandom from './events/onRandom.js';
 import onDocumentByRef from './events/onDocumentByRef.js';
-import onSync from './events/onSync.js'
+import onOfferedDocuments from './events/onOfferedDocuments.js'
 
 
 export default {
@@ -10,5 +10,5 @@ export default {
     onSearch : onSearch,
     onRandom : onRandom,
     onDocumentByRef : onDocumentByRef,
-    onSync : onSync
+    onOfferedDocuments : onOfferedDocuments
 };
